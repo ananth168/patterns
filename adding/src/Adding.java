741dd68798dea502f44bfa0bec60d89441a1c02e
Ananthram <Ananthram@DESKTOP-ABU1UMQ>
2020-06-19 13:07:10 +0530

    commited

---
+++ adding/src/Adding.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter a number :");
+		double a= scan.nextDouble();
+		System.out.println("enter another number :");
+		double b= scan.nextDouble();
+		
+		
+		System.out.println(a+b);
+	}
+
+}
